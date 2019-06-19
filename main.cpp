be5a93c3ee361f7b195c8aaf2dd89ddd932d70d9
TVasquez254 <tvasquez@austincc.edu>
2019-06-18 22:28:33 -0500

    msg

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/***********************************************************
+Name: Tomas Vasquez 
+Assignment: 03
+Purpose: This program collection of nodes that are Self referential node
+***********************************************************/
+#include "main.h"
+
+int main(int argc, char** argv)
+{
+
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+/***********************************************************
+Name: Tomas Vasquez 
+Assignment: 03
+Purpose: This program collection of nodes that are Self referential node
+***********************************************************/
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>     /* cout, endl */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+using namespace std;
+
+#endif /* MAIN_H */
