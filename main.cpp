ee38fff500416b3203e491f269f9ede6b4b9ed3e
TVasquez254 <tvasquez@austincc.edu>
2019-06-19 20:05:50 -0500

    second push

--- main.cpp
+++ main.cpp
@@ -1,12 +1,88 @@
 /***********************************************************
 Name: Tomas Vasquez 
 Assignment: 03
-Purpose: This program collection of nodes that are Self referential node
+Purpose: This program functional doubly linked list class as described in class and in your text. Create a
+linked list object from your class. This will require random data and numbers. g++ -I ./ *.cpp
 ***********************************************************/
-#include "main.h"
+#include<iostream>
+#include<string>
+#include <cstdlib>
 
-int main(int argc, char** argv)
+using namespace std;
+struct Node
 {
+    int id;
+    string data;
+    Node *back;
+    Node *forward;
+};
+
+class Items
+{
+    private:
+    int count;
+    Node *head;
+
+    public: 
+    Items();
+    ~Items();
+    bool addNode(int, string);
+    bool deleteNode(int);
+    //bool getNode(int, DataNode*);
+    void printList(bool);
+    int getCount();
+    bool clear();
+};
+
+Items::Items()
+{
+    head = nullptr ;
+}
+
+Items::~Items()
+{
+    
+}
+
+bool Items::addNode(int x, string data)
+{
+    bool success = false;
+    if (head ==NULL)
+    {
+        Node *tmp = new Node;
+        tmp -> id = x;
+        tmp -> data = data;
+        tmp -> forward = NULL;
+        tmp -> back = NULL;
+        head = tmp;
+        cout<<tmp -> id<<" "<<tmp -> data<<endl;
+        //count++;
+        success = true;
+    }
+    else 
+    {
+
+
+    }
+    return success;
+
+}
+
+void Items::printList(bool)
+{
+    cout << head->id << endl;
+
+}
+
+
+int main()
+{
+    Items a;
+    a.addNode(5, "t");
+    //a.addNode(6, "t");
+    //a.printList(a.addNode(1, "t"));
+    
+
 
 
     return 0;
